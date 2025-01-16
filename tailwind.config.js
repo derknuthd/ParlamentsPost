ee3f7d590d866e80cd152b37c3a3e4d9cccf92b3
derknuthd <c+github@trn.pm>
2025-01-17 00:40:31 +0100

    darkmode funktioniert weitestgehend

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 // tailwind.config.js
 module.exports = {
+  // Aktiviert den Dark Mode über die 'class' Strategie
+  darkMode: 'class', // Optionen: 'media' oder 'class'
+
+  // Pfade zu allen Dateien, die Tailwind CSS verwenden
   content: [
-    "./frontend/**/*.{html,js}",
-    "./backend/**/*.{js,jsx,ts,tsx}"
+    "./frontend/**/*.{html,js,jsx,ts,tsx}",
+    "./backend/**/*.{js,jsx,ts,tsx}",
   ],
+
   theme: {
-    extend: {},
+    extend: {
+      // Hier kannst du benutzerdefinierte Theme-Erweiterungen hinzufügen
+      // Beispiel: Farben, Schriftarten, etc.
+    },
   },
-  plugins: [],
-}
+
+  plugins: [
+    // Hier kannst du Tailwind CSS Plugins hinzufügen, falls benötigt
+  ],
+};
